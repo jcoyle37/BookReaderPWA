0d7a5e3c3aab25a58c99ecfdf360c254adf06b25
Joe Coyle <jcoyle37@gmail.com>
2019-07-31 10:22:41 -0400

    download button disabled on click

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,115 +2,22 @@ import React from 'react';
 import { getDataUri, setLocalStorage, modifyLibraryList } from '../scripts/general.js';
 import $ from 'jquery';
 
-function SearchResults(props) {
-  if(!props.query) //default view, no searches performed
-    return (
-      <div>
-      </div>
-    )
-  else if(props.loading) //if loading search query
-    return (
-      <div>
-        Loading...
-      </div>
-    );
-  else { //display search query
-    return (
-      <div>
-        <h2>{props.results.length} Results for {props.query}</h2>
-        <ul className='bookResults'>
-          {props.results.map((book) => (
-            <li key={'bookResult_' + book.identifier}>
-              <a href={`https://archive.org/details/${book.identifier}`}
-                 target="_blank"
-                 rel="noopener noreferrer">
-                <img
-                  src={`https://archive.org/services/img/${book.identifier}`}
-                  alt=''
-                />
-                <p>
-                  {book.title}
-                </p>
-              </a>
-              <button onClick={() => props.onDownloadBook(book.identifier)}>Download</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
-class Search extends React.Component {
+class DownloadBtn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      input: '', //current query input
-      lastQuery: '', //last search query performed
-      searchResults: [],
-      loading: false //set 'true' when loading a search query
+      disabled: false //set 'true' when downloading
     };
-
-    this.updateInput = this.updateInput.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-
-  updateInput(e) { //as user types into query input, update text
-    const value = e.target.value;
-
-    this.setState({
-      input: value
-    });
-  }
-
-  handleSearch() { //triggered by clicking 'download' button
-    this.setState((currentState) => {
-      return {
-        loading: true,
-        lastQuery: currentState.input
-      }
-    });
-
-    const maxRows = 25; //set to 0 for default (50); setting lower reduces server load
-    let query = //construct search URL (determined from https://archive.org/advancedsearch.php)
-      '///archive.org/advancedsearch.php?q=%28'
-      + this.state.input + '%29%20AND%20mediatype%3A%28texts%29'
-      + '%20AND%20-collection%3A%28printdisabled%29' //selects books that don't require borrowing
-      + '&fl%5B%5D=creator&fl%5B%5D=description&fl%5B%5D=identifier'
-      + '&fl%5B%5D=item_size&fl%5B%5D=publisher&fl%5B%5D=title'
-      + '&fl%5B%5D=year&sort%5B%5D=downloads+desc&sort%5B%5D=&sort&page=1'
-      + '&output=json&callback=IAE.search_hits';
-    if(maxRows > 0)
-      query += '&rows=' + maxRows;
-  
-    //perform search
-    new Promise((resolve, reject) => {
-			$.ajax({
-        url: query,
-        type: 'GET',
-        dataType: 'jsonp',
-        success: function(res) {
-          resolve(res);
-        },
-        error: function(jqXHR) {
-          reject(jqXHR);
-        }
-      });
-		}).then((res) => {
-      this.setState(() => {
-        return {
-          searchResults: res.response.docs,
-          loading: false
-        }
-      });
-		}).catch((error) => {
-      console.log(error);
-		});
   }
 
   handleDownloadBook(id) {
-    //todo: disable download button and future books being downloaded while this is happening
+    //todo: set disabled = true always if already in library
+
+    //disable download button on click
+    this.setState({disabled: true});
+
+    //todo: disable other books being downloaded while this is happening
     new Promise((resolve, reject) => {
       //get book metadata which will provide us with information needed to construct
       //a URL we can use to call BookReaderJSIA.php, which will provide book leaf URIs
@@ -209,6 +116,127 @@ class Search extends React.Component {
         console.log(error);
       });
     });
+  }
+
+  render() {
+    const downloadBtnTxt = this.state.disabled ? 'Please wait...' : 'Download';
+
+    return (
+      <button
+        onClick={() => this.handleDownloadBook(this.props.identifier)}
+        disabled={this.state.disabled}
+      >{downloadBtnTxt}</button>
+    );
+  }
+}
+
+function SearchResults(props) {
+  if(!props.query) //default view, no searches performed
+    return (
+      <div>
+      </div>
+    )
+  else if(props.loading) //if loading search query
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  else { //display search query
+    return (
+      <div>
+        <h2>{props.results.length} Results for {props.query}</h2>
+        <ul className='bookResults'>
+          {props.results.map((book) => (
+            <li key={'bookResult_' + book.identifier}>
+              <a href={`https://archive.org/details/${book.identifier}`}
+                 target="_blank"
+                 rel="noopener noreferrer">
+                <img
+                  src={`https://archive.org/services/img/${book.identifier}`}
+                  alt=''
+                />
+                <p>
+                  {book.title}
+                </p>
+              </a>
+              <DownloadBtn
+                identifier = {book.identifier}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '', //current query input
+      lastQuery: '', //last search query performed
+      searchResults: [],
+      loading: false //set 'true' when loading a search query
+    };
+
+    this.updateInput = this.updateInput.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  updateInput(e) { //as user types into query input, update text
+    const value = e.target.value;
+
+    this.setState({
+      input: value
+    });
+  }
+
+  handleSearch() { //triggered by clicking 'download' button
+    this.setState((currentState) => {
+      return {
+        loading: true,
+        lastQuery: currentState.input
+      }
+    });
+
+    const maxRows = 25; //set to 0 for default (50); setting lower reduces server load
+    let query = //construct search URL (determined from https://archive.org/advancedsearch.php)
+      '///archive.org/advancedsearch.php?q=%28'
+      + this.state.input + '%29%20AND%20mediatype%3A%28texts%29'
+      + '%20AND%20-collection%3A%28printdisabled%29' //selects books that don't require borrowing
+      + '&fl%5B%5D=creator&fl%5B%5D=description&fl%5B%5D=identifier'
+      + '&fl%5B%5D=item_size&fl%5B%5D=publisher&fl%5B%5D=title'
+      + '&fl%5B%5D=year&sort%5B%5D=downloads+desc&sort%5B%5D=&sort&page=1'
+      + '&output=json&callback=IAE.search_hits';
+    if(maxRows > 0)
+      query += '&rows=' + maxRows;
+  
+    //perform search
+    new Promise((resolve, reject) => {
+			$.ajax({
+        url: query,
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function(res) {
+          resolve(res);
+        },
+        error: function(jqXHR) {
+          reject(jqXHR);
+        }
+      });
+		}).then((res) => {
+      this.setState(() => {
+        return {
+          searchResults: res.response.docs,
+          loading: false
+        }
+      });
+		}).catch((error) => {
+      console.log(error);
+		});
   }
 
   render() {
